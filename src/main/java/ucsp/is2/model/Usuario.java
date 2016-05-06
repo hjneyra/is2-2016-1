98ef3ec97f307fec3c3f83a2cd98c8a3b47f8ecb
christianfm10 <christian.flores@ucsp.edu.pe>
2016-05-06 17:16:06 -0500

    Creacion del modelo usuario

---
+++ src/main/java/ucsp/is2/model/Usuario.java
@@ -0,0 +1,51 @@
+package ucsp.is2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@SequenceGenerator(name="USUARIO_ID_GENERATOR", sequenceName="USUARIO_ID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="USUARIO_ID_GENERATOR")
+	private Long id;
+
+	private String name;
+
+    private String last_name;
+
+    private String email;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return last_name;
+	}
+
+	public void setLastname(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}

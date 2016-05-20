3bda516b81d3e678526c341b26b26466582f6bcc
WilbertAlfonso <wilbert.marroquin@ucsp.edu.pe>
2016-05-20 18:56:22 -0500

    Añadido Listar Usuarios

--- src/main/java/ucsp/is2/repository/UsuarioRepository.java
+++ src/main/java/ucsp/is2/repository/UsuarioRepository.java
@@ -1,26 +1,12 @@
 package ucsp.is2.repository;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.Repository;
 
 import ucsp.is2.model.Usuario;
-@Repository
-public class UsuarioRepository {
-	@PersistenceContext
-	public EntityManager entityManager;
 
-	public List<Usuario> findAll() {
-		TypedQuery<Usuario> query = entityManager.createQuery("SELECT c FROM public.usuario c", 							Usuario.class);
-		List<Usuario> results = query.getResultList();
-		System.out.println(results.size());
-		return results;
-	}
+public interface UsuarioRepository extends Repository<Usuario, Long> {
+	public Usuario findById(Long id);
+	public List<Usuario> findAll();
 }

9891fa8f41134ee539a383ec478dc25572641544
luisandia <luis.andia@ucsp.edu.pe>
2016-06-25 14:55:22 -0500

    segundo test de integracion

--- src/main/java/ucsp/is2/rest/TestController.java
+++ src/main/java/ucsp/is2/rest/TestController.java
@@ -1,15 +1,15 @@
 package ucsp.is2.rest;
 
-import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ucsp.is2.service.TestService;
 
-import java.util.Map;
-import java.util.HashMap;
+import ucsp.is2.service.TestService;
 
 @RestController
 public class TestController {

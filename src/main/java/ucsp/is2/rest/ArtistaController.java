91d11ea67dfa1a9648938491914d17f0b832988e
rodrigoManx <rodrigo.pulcha@ucsp.edu.pe>
2016-07-08 18:01:22 -0500

    Agregado el artistaControllertest con las pruebas al respectivo controllador

--- src/main/java/ucsp/is2/rest/ArtistaController.java
+++ src/main/java/ucsp/is2/rest/ArtistaController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ucsp.is2.model.Artista;
 import ucsp.is2.repository.ArtistaRepository;
 
-
 class Log_Artista {
 	public String name;
 	public String genero;
@@ -31,46 +30,41 @@ public class ArtistaController {
 	@RequestMapping(value = "/saveArtista", method = RequestMethod.POST)
 	public HttpEntity<Artista> saveArtista(@RequestBody Log_Artista log) {
 		System.out.println(log.name);
-		Artista artista; //= repository.findByName(log.name);
-		
-		//if(Artista == null){
-			artista = new Artista(log.name,log.genero,log.descripcion,log.ciudad,log.pais);
-			repository.save(artista);
-			//return Artista;
-			return new HttpEntity<Artista>(artista);
-		/*}
-		return null;*/
+		Artista artista; // = repository.findByName(log.name);
+		artista = new Artista(log.name, log.genero, log.descripcion,
+				log.ciudad, log.pais);
+		repository.save(artista);
+		return new HttpEntity<Artista>(artista);
+
 	}
-	
+
 	@RequestMapping(value = "/delArtista", method = RequestMethod.POST)
 	public HttpEntity<Artista> delArtista(@RequestBody Long idartista) {
 		System.out.println(idartista);
-		Artista artista; //= repository.findByName(log.name);
+		Artista artista;
 		
-		//if(Artista == null){
-			artista = repository.findById(idartista);
-			repository.delete(artista);
-			//return Artista;
-			
-			return new HttpEntity<Artista>(artista);
-		/*}
-		return null;*/
+		artista = repository.findById(idartista);
+		repository.delete(artista);
+
+		return new HttpEntity<Artista>(artista);
+
 	}
-	
-	
-	
+
 	/*
 	 * function listar usuarios llamada desde usuario en el frontEnd
-	 * @author LuisAndia 
-	 * */
+	 * 
+	 * @author LuisAndia
+	 */
 	@RequestMapping(value = "artistas", method = RequestMethod.GET)
 	public List<Artista> listAll() {
 		return repository.findAll();
 	}
+
 	/*
 	 * lista un usuario especifico ejm: usuario?id=1
-	 * @author LuisAndia 
-	 * */
+	 * 
+	 * @author LuisAndia
+	 */
 	@RequestMapping(value = "artista/{id}")
 	public HttpEntity<Artista> listAll(@PathVariable Long id) {
 		Artista Artista = repository.findById(id);
@@ -82,8 +76,9 @@ public class ArtistaController {
 
 	/*
 	 * editar un artista especifico ejm: artista?id=1
-	 * @author LuisAndia 
-	 * */
+	 * 
+	 * @author LuisAndia
+	 */
 	@RequestMapping(value = "edit_artista/{id}")
 	public HttpEntity<Artista> edit_artista(@PathVariable Long id) {
 		Artista Artista = repository.findById(id);
@@ -92,6 +87,5 @@ public class ArtistaController {
 		}
 		return new HttpEntity<Artista>(Artista);
 	}
-	
-	
+
 }

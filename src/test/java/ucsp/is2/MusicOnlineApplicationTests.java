7f8c5e2aa16d8704a8938097a800e19cc3ff7108
rodrigosantacruz <rodrigo.santacruz@ucsp.edu.pe>
2016-07-08 19:14:54 -0500

    Eliminado test innecesarios

--- src/test/java/ucsp/is2/MusicOnlineApplicationTests.java
+++ src/test/java/ucsp/is2/MusicOnlineApplicationTests.java
@@ -7,23 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -56,10 +40,6 @@ public class MusicOnlineApplicationTests {
 	
 	@Autowired
 	UsuarioController cu;
-	
-	
-	
-	private MockMvc mockMvc;
 
 	
 	@Test
@@ -71,55 +51,6 @@ public class MusicOnlineApplicationTests {
 	}
 
 
-	/*
-	 * test unitario para verificar si el usuario ingresado
-	 * es correcto
-	 * */
-	@Test
-	public void testLogin() {
 
-		
-		Usuario user = new Usuario();
-		Random ram= new Random();
-		user.setPassword("123");
-		user.setEmail("ework@gmail.com"+ram.nextFloat());
-		user.setLastname("Team2");
-		user.setName("ework");
-		
-		usuarioRepository.save(user);
-		id=user.getId();
-		Assert.assertNotNull(id);
-
-		Usuario user2 = usuarioRepository.findById(id);
-		
-		Usuario usuario =loginService.login(user2.getEmail(),user2.getPassword());
-		
-		Assert.assertNotNull(usuario);
-		
-		Usuario usuario1 =loginService.login("luis","12345");
-		Assert.assertNull(usuario1);
-		
-		
-		//Usuario user3 = new Usuario("ework","ework_last_name","ework@gmail.com"+ram.nextFloat(),"123");
-		//Assert.assertNotNull(user3);
-	}
-
-	@Test
-	public void UsuarioTest()
-	{
-		//UsuarioController cu = new UsuarioController();
-		
-		/*cu.Log_Usuario c = new cu.Log_Usuario();
-		c.name="ework";
-		c.last_name="ework lastname";
-		c.email="ework@gmail.com";
-		c.password="123";
-		Usuario res= cu.save(c);*/
-		
-		
-		HttpEntity<Usuario> user = cu.listAll(1L);
-		Assert.assertNotNull(user);
-
-	}
 
 }
